c26427c0f9abb173a5b688c918130bf456ccd182
KarimAgliullin <agliullinkarim1@gmail.com>
2024-12-24 19:03:08 +0300

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,6 +1,10 @@
-name='Napoleon'
-print(name[:0])
-print(name[-1])
-print(len(name))
-print(name[::-1])
-print(name[1:2])
+my_string = input("Введите любой текст: ")
+print("Количество символов в тексте:", len(my_string))
+print("Текст в верхнем регистре:", my_string.upper())
+print("Текст в нижнем регистре:", my_string.lower())
+my_string_no_spaces = my_string.replace(" ", "")
+print("Текст без пробелов:", my_string_no_spaces)
+if my_string:
+    print("Первый символ строки:", my_string[0])
+if my_string:
+    print("Последний символ строки:", my_string[-1])
